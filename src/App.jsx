272e759211d87fa08f9a8dc6813aa0a78987c668
Vaibhav Chalapakula <vaibhavch304@gmail.com>
2025-01-16 22:56:50 +0530

    INIT COMMIT: Project setup.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import { Button } from "@/components/ui/button";
+import Layout from "@/components/Layout";
+import TopBar from "./components/TopBar";
+
+function App() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
